9b2f88da6d0ec23f9147376c7635eddf158fc873
Om Gupta <your_email@example.com>
2026-01-16 18:23:15 +0530

    add redirects

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,7 +27,7 @@ import ViewLecture from './pages/ViewLecture'
 import SearchWithAi from './pages/SearchWithAi'
 import getAllReviews from './customHooks/getAllReviews'
 
-export const serverUrl = "http://localhost:8000"
+export const serverUrl = "https://ai-lms-website-63tw.onrender.com"
 
 function App() {
   
